3e7bdd530e84c05d86d11fdc4adb75082613cad4
amandeep-singh-parihar <amandeepparihar24@gmail.com>
2024-06-12 12:53:13 +0530

    project done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Testimonials from "./Components/Testimonials";
+import reviews from "./Components/data";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className="h-screen flex flex-col w-[100vw] justify-center items-center bg-gray-200">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">Our Testimonials</h1>
+        <div className="bg-violet-400 h-[3px] w-1/5 mt-2 mx-auto"></div>
+        <Testimonials reviews={reviews}/>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
