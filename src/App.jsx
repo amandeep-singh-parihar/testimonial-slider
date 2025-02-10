69539ffe314b3c29ea3e1681a92e4689a8dee89f
amandeep-singh-parihar <amandeepparihar24@gmail.com>
2025-02-10 18:22:54 +0530

    Revision + documentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Testimonials from "./Components/Testimonials";
-import reviews from "./Components/data";
+import reviews from "./Components/data"; // fetching all the review from the data.jsx
 
 function App() {
-  return (
-    <div className="h-screen flex flex-col w-[100vw] justify-center items-center bg-gray-200">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold">Our Testimonials</h1>
-        <div className="bg-violet-400 h-[3px] w-1/5 mt-2 mx-auto"></div>
-        <Testimonials reviews={reviews}/>
-      </div>
-    </div>
-  );
+    return (
+        <div className="h-screen flex flex-col w-[100vw] justify-center items-center bg-gray-200">
+            <div className="text-center">
+                <h1 className="text-4xl font-bold">Our Testimonials</h1>
+                <div className="bg-violet-400 h-[3px] w-1/5 mt-2 mx-auto"></div>
+                <Testimonials reviews={reviews} />
+                {/* send the review data to the testimonials components */}
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -3,59 +3,65 @@ import Card from "./Card";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
 function Testimonials({ reviews }) {
-  const [index, setIndex] = useState(0);
+    // retrive the reviews data from app.jsx using props
+    const [index, setIndex] = useState(0); // intial the index is at 0
 
-  function leftShiftHandler() {
-    if (index - 1 < 0) {
-      setIndex(reviews.length - 1);
-    } else {
-      setIndex(index - 1);
+    function leftShiftHandler() {
+        // this wraps the array
+        if (index - 1 < 0) {
+            // when the left button pressed if the index-1 < 0 it means we want we want to access the last index simply setIndex(array.lengt-1)
+            setIndex(reviews.length - 1);
+        } else {
+            setIndex(index - 1); // else simplye setIndex(index-1);
+        }
     }
-  }
 
-  function rightShiftHandler() {
-    if (index + 1 >= reviews.length) {
-      setIndex(0);
-    } else {
-      setIndex(index + 1);
+    function rightShiftHandler() {
+        if (index + 1 >= reviews.length) {
+            // when the right button pressed if the index+1>=reviews.length it means we want to reach at the 0th index from the behind simple put the setIndex(0)
+            setIndex(0);
+        } else {
+            setIndex(index + 1); // else increase the index+1
+        }
     }
-  }
 
-  function surpriseHandler() {
-    let randomIndex = Math.floor(Math.random() * reviews.length);
-    setIndex(randomIndex);
-  }
+    function surpriseHandler() {
+        let randomIndex = Math.floor(Math.random() * reviews.length);
+        setIndex(randomIndex);
+        // This is the randomIndex using the Math.flor function
+    }
 
-  return (
-    <div className="w-[85vw] md:w-[700px] bg-white flex flex-col justify-center items-center mt-10 p-10 duration-700 rounded-md hover:shadow-2xl transition-all ease-in-out">
-      <Card review={reviews[index]} />
+    return (
+        <div className="w-[85vw] md:w-[700px] bg-white flex flex-col justify-center items-center mt-10 p-10 duration-700 rounded-md hover:shadow-2xl transition-all ease-in-out">
+            <Card review={reviews[index]} />
+            {/* send the reviews[index] a specifc index to the card */}
 
-      <div className="flex text-3xl mt-8 gap-3 text-violet-400 font-bold justify-center">
-        <button
-          onClick={leftShiftHandler}
-          className="cursor-pointer hover:text-violet-500"
-        >
-          <MdKeyboardArrowLeft />
-        </button>
+            <div className="flex text-3xl mt-8 gap-3 text-violet-400 font-bold justify-center">
+                <button
+                    onClick={leftShiftHandler}
+                    className="cursor-pointer hover:text-violet-500"
+                >
+                    <MdKeyboardArrowLeft />
+                </button>
 
-        <button
-          onClick={rightShiftHandler}
-          className="cursor-pointer hover:text-violet-500"
-        >
-          <MdKeyboardArrowRight />
-        </button>
-      </div>
+                <button
+                    onClick={rightShiftHandler}
+                    className="cursor-pointer hover:text-violet-500"
+                >
+                    <MdKeyboardArrowRight />
+                </button>
+            </div>
 
-      <div className="mt-4">
-        <button
-          onClick={surpriseHandler}
-          className="bg-violet-400 hover:bg-violet-500 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg"
-        >
-          Surprise Me
-        </button>
-      </div>
-    </div>
-  );
+            <div className="mt-4">
+                <button
+                    onClick={surpriseHandler}
+                    className="bg-violet-400 hover:bg-violet-500 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg"
+                >
+                    Surprise Me
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default Testimonials;
